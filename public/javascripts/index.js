84a7163130e95031c2866cdf0d2c531f79085c38
Ben Myers <bmyers@appnexus.com>
2013-09-28 17:50:29 -0400

    Index allows stress and comfort ot be deleted

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
 	getAllStress(function(stress) {
 		stress.forEach(function(stressor) {
-			$('<p></p>').text(stressor.text).appendTo('body');
+			$('<p>' + stressor.text + ' <a class="delete-stressor" href="#' + stressor.id + '">delete</a></p>').appendTo('body').find('a');
 			if(stressor.comfort.length > 0) {
 				var ul = $('<ul></ul>').appendTo('body');
 				stressor.comfort.forEach(function(comfort) {
-					$('<li></li>').text(comfort.text).appendTo(ul);
+					$('<li>' + comfort.text + ' <a class="delete-comfort" href="#' + comfort.id + '">delete</a></li>').appendTo(ul);
 				});
 			}
 		});
+	});
+	$('body').on('click', 'a', function() {
+		var p = $(this).parent();
+		var id = $(this).attr('href');
+		id = id.substr(1, id.length - 1);
+		if($(this).hasClass('delete-comfort')) {
+			deleteComfort(id, function(successful) {
+				p.remove();
+			});
+		}
+		else {
+			deleteStress(id, function(successful) {
+				p.remove();
+			});
+		}
 	});
 });
