4119a823f66558b2d92f4dd6af9684860be5f488
MattyLapage <matthewclepage@gmail.com>
2014-01-08 15:17:24 -0500

    Updating DB pointer for heroku

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 //if it becomes necessary, this is where changing configs based on NODE_ENV should be performed
 module.exports = {
 	db: {
-		uri: (process.env.DB_URI || 'mongodb://localhost/test')
+		uri: (process.env.DB_URI || System.getenv("MONGOHQ_URL"))
 	},
 	session: {
 		secret: (process.env.SESSION_SECRET || 'asupersecuresecret')
