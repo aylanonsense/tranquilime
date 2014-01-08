14e8fe19cb9a238b585c922d68a18d867917e68d
MattyLapage <matthewclepage@gmail.com>
2014-01-08 15:46:40 -0500

    Experimenting with mongoose/heroku interactions

--- config/config.js
+++ config/config.js
@@ -1,7 +1,9 @@
 //if it becomes necessary, this is where changing configs based on NODE_ENV should be performed
 module.exports = {
 	db: {
-		uri: (System.getenv("MONGOHQ_URL"))
+		uri: (  process.env.MONGOLAB_URI ||
+                process.env.MONGOHQ_URL ||
+                'mongodb://localhost/HelloMongoose')
 	},
 	session: {
 		secret: (process.env.SESSION_SECRET || 'asupersecuresecret')
